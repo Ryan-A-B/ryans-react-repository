5cc6f5b809b2f5a2cdb825ea6c55cdd580827cff
Ryan <ryan.a.ballinger@gmail.com>
2017-09-27 11:16:32 +1000

    Added Input

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var Form = require("./dist/Form.js");
 var Modal = require("./dist/Modal.js");
 var WithFocus = require("./dist/WithFocus.js");
 var Dropdown = require("./dist/Dropdown.js");
+var Input = require("./dist/Input.js");
 
 module.exports = {
     Button,
     Form,
     Modal,
     WithFocus,
-    Dropdown
+    Dropdown,
+    Input
 };
---
+++ src/Input.js
@@ -0,0 +1,32 @@
+export function Input ({className, onChange, ...attributes}) {
+    Object.assign(attributes, {
+        className: "form-control",
+        onChange: (e) => {onChange($(e.target).val());}
+    });
+
+    return <input {...attributes}/>
+}
+
+export function Text ({...attributes}) {
+    Object.assign(attributes, {
+        type: "text"
+    });
+
+    return <Input {...attributes}/>
+}
+
+export function Email ({...attributes}) {
+    Object.assign(attributes, {
+        type: "email"
+    });
+
+    return <Input {...attributes}/>
+}
+
+export function Number ({...attributes}) {
+    Object.assign(attributes, {
+        type: "number"
+    });
+
+    return <Input {...attributes}/>
+}
